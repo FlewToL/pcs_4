6603d3403f1a7d4e298d5e3600c82cc6291a9879
RoMango <112846459+FlewToL@users.noreply.github.com>
2024-06-13 13:54:53 +0300

    Add files via upload

---
+++ Database/OdbConnectContex.cs
@@ -0,0 +1,31 @@
+﻿using Corpa4Sem4.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace PraktASPApp.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Message>? Messages { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+			var options = new
+			{
+				Server = "127.0.0.1",
+				Database = "pcs_4",
+				User = "postgres",
+				Password = "123456",
+                Port = "5441"
+			};
+
+			optionsBuilder.UseNpgsql($"Server = {options.Server}; Database ={options.Database}; Uid = {options.User}; Pwd = {options.Password}; Port = {options.Port}");
+		}
+    }
+}
